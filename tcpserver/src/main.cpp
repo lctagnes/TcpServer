9dd9bf2e78af531c69a728d56fc92f04b31af6b5
lctagnes <378640378@qq.com>
2022-01-25 20:58:02 +0800

    基于TCP的服务器和客户端

--- tcpserver/src/main.cpp
+++ tcpserver/src/main.cpp
@@ -1,16 +1,97 @@
 #include <iostream>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <cstring>
+#include <sys/socket.h>
 #include "base.h"
+
+//出错调用函数
+static void error_handle(std::string opt, std::string message)
+{
+    //根据errno值获取失败原因并打印到终端
+    perror(opt.c_str());
+    std::cout << message << std::endl;
+    exit(1);
+}
 
 int main(int argc, char *argv[])
 {
-    char username[BUFFSIZE] = {0};
-    std::cout << "Hello world" << std::endl;
+    int serv_sock, client_sock;
+    char message[BUFFSIZE];
+    int str_len, ret;
 
-    std::cout << "Please Input Your Name :" <<std::endl;
+    struct sockaddr_in serv_adr, client_adr;
+    socklen_t client_adr_size;
 
-    std::cin >> username;
+    //判断命令行参数合法性
+    if(argc < 2)
+    {
+        std::cout << "Usage : " << argv[0] << " <port>" << std::endl;
+        exit(1);
+    }
 
-    std::cout << "Hi! " << username << " , Welcome China !" << std::endl;
+    //创建socket套接字
+    serv_sock = socket(PF_INET, SOCK_STREAM, 0);
+    if(serv_sock < 0)
+    {
+        error_handle("socket", "socket() error.");
+    }
 
+    //初始化服务器套接字参数，设置网卡IP 和端口号
+    memset(&serv_adr, 0, sizeof(serv_adr));
+    serv_adr.sin_family = AF_INET;
+    serv_adr.sin_addr.s_addr = htonl(INADDR_ANY);
+    serv_adr.sin_port = htons(atoi(argv[1]));
+
+    //绑定端口
+    ret = bind(serv_sock, (struct sockaddr*)&serv_adr, sizeof(serv_adr));
+    if(ret < 0)
+    {
+        error_handle("bind", "bind() error.");
+    }
+
+    //监听TCP端口号
+    ret = listen(serv_sock, 5);
+    if(ret < 0)
+    {
+        error_handle("listen", "listen() error.");
+    }
+
+    client_adr_size = sizeof(client_adr);
+
+    while(1)
+    {
+        //使用accept接收客户端连接请求
+        client_sock = accept(serv_sock, (struct sockaddr*)&client_adr, &client_adr_size);
+        if(client_sock < 0)
+        {
+            //接收客户端连接请求失败，
+            continue;
+        }
+        //接收新的客户端请求，进行客户端数据处理
+        std::cout << "Accept New Client : " << inet_ntoa(client_adr.sin_addr) << " , port : " << ntohs(client_adr.sin_port) << std::endl;
+        std::cout << "Start Recv Client Data..." << std::endl;
+
+        //清空缓存区
+        memset((void *)&message, 0, BUFFSIZE);
+        while((str_len = read(client_sock, message, BUFFSIZE)) != 0)
+        {
+            //成功读取客户端发送来的数据消息
+            //打印输出
+            std::cout << "Recv Message : " << message << std::endl;
+            //将消息回传给客户端，作为回声服务器，类似 echo 命令
+            write(client_sock, message, str_len);
+            //清空缓存区，等待再次读取数据
+            memset(message, 0, BUFFSIZE);
+        }
+
+        //客户端断开连接，关闭套接字
+        close(client_sock);
+    }
+
+    //服务器关闭，关闭服务器套接字
+    close(serv_sock);
     return 0;
 }
---
+++ tcpserver/test/TcpClient.cpp
@@ -0,0 +1,74 @@
+#include <iostream>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <cstring>
+#include <sys/socket.h>
+#include "base.h"
+
+//出错调用函数
+static void error_handle(std::string opt, std::string message)
+{
+    //根据errno值获取失败原因并打印到终端
+    perror(opt.c_str());
+    std::cout << message << std::endl;
+    exit(1);
+}
+
+int main(int argc, char *argv[])
+{
+    int sock, ret;
+    char message[BUFFSIZE];
+    int str_len;
+    struct sockaddr_in serv_adr;
+
+    if(argc < 3)
+    {
+        std::cout << "Usage : " << argv[0] << " <IP> <port>" << std::endl;
+        exit(1);
+    }
+
+    sock = socket(PF_INET, SOCK_STREAM, 0);
+    if(sock < 0)
+    {
+        error_handle("socket", "socket() error.");
+    }
+
+    memset(&serv_adr, 0, sizeof(serv_adr));
+    serv_adr.sin_family = AF_INET;
+    serv_adr.sin_addr.s_addr = inet_addr(argv[1]);
+    serv_adr.sin_port = htons(atoi(argv[2]));
+
+    ret = connect(sock, (struct sockaddr*)&serv_adr, sizeof(serv_adr));
+    if(ret < 0)
+    {
+        error_handle("connect", "connect() error.");
+    }
+
+    std::cout << "Connect Success..." << std::endl;
+
+    //进入数据处理
+    while(1)
+    {
+        std::cout << "Please Input Message(Q to quit) : " << std::endl;
+        std::cin >> message;
+        if((!strcmp(message, "q")) || (!strcmp(message, "Q")))
+        {
+            //退出客户端
+            break;
+        }
+
+        //将数据发送给服务端
+        write(sock, message, strlen(message));
+        //读取服务端回传的数据
+        str_len = read(sock, message, BUFFSIZE-1);
+        message[str_len] = 0;
+        //打印输出
+        std::cout << "Echo Message : " << message << std::endl;
+    }
+
+    //关闭套接字
+    close(sock);
+    return 0;
+}
